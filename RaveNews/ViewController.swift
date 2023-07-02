2752cef5a1e89afd79bfef919a2158760282781e
Berkin <bkabadayi@ziraatteknoloji.com>
2023-07-02 19:21:49 +0300

    Initial commit

---
+++ RaveNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RaveNews
+//
+//  Created by Berkin Kabadayı on 2.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
